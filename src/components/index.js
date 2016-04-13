ee5ae6bca499a79e137a4869a0af0b3894f34e0d
renevanderark <r.van.der.ark@gmail.com>
2016-04-13 13:35:26 +0200

    implement login

--- src/components/index.js
+++ src/components/index.js
@@ -8,14 +8,14 @@ import LoginForm from "./login-form";
 class App extends React.Component {
 
 	render() {
-		const {letters, current} = this.props.letters;
+		const { letters, current } = this.props.letters;
 		const { username } = this.props.user;
 
 		return (
 			<div className="app">
 				{username === null ? <LoginForm {...this.props} /> : `Logged in as ${username}`}
 
-				<Letter current={current} letter={letters[current]} onSelect={this.props.onSelect} total={letters.length - 1} />
+				<Letter {...this.props} current={current} letter={letters[current]} onSelect={this.props.onSelect} total={letters.length - 1} />
 {/*				<KeywordSuggest {...this.props} /> */}
 			</div>
 		);
@@ -24,8 +24,8 @@ class App extends React.Component {
 
 App.propTypes = {
 	letters: React.PropTypes.object,
-	user: React.PropTypes.object,
-	onSelect: React.PropTypes.func
+	onSelect: React.PropTypes.func,
+	user: React.PropTypes.object
 };
 
 export default App;
